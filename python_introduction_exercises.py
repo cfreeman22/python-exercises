b8bb774fdff004a15a5955bc012600434cbc3466
Christian Freeman <freemanchristian410@gmail.com>
2021-09-30 16:19:53 -0500

    Testing python

---
+++ python_introduction_exercises.py
@@ -0,0 +1,9 @@
+
+# 1 Open Visual Studio Code and install the Python extension. This will provide nice editing features when writing Python code.
+#Ensure that you have the correct version of Python installed by running the command in this lesson that prints the version 
+# of Python that you have installed.
+# Create a hello world program. Create a text file named python_introduction_exercises.py
+
+
+print("Hello, World!")
+
